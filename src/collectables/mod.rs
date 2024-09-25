833675a93c39b3a43655b84147d663613f5eb9bd
claudijo <claudijo.borovic@gmail.com>
2024-09-25 20:37:20 +0300

    Add level states

--- src/collectables/mod.rs
+++ src/collectables/mod.rs
@@ -5,14 +5,15 @@ use crate::collectables::systems::{
     collect_money, spawn_money_trail_1, spawn_money_trail_2, spawn_money_trail_3,
 };
 use bevy::prelude::*;
+use crate::level::LevelState;
 
 pub struct CollectablesPlugin;
 
 impl Plugin for CollectablesPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_money_trail_1);
-        app.add_systems(Startup, spawn_money_trail_2);
-        app.add_systems(Startup, spawn_money_trail_3);
+        app.add_systems(OnEnter(LevelState::FirstWave), spawn_money_trail_1);
+        app.add_systems(OnEnter(LevelState::SecondWave), spawn_money_trail_2);
+        app.add_systems(OnEnter(LevelState::ThirdWave), spawn_money_trail_3);
         app.add_systems(Update, collect_money);
     }
 }
--- src/level/mod.rs
+++ src/level/mod.rs
@@ -3,10 +3,18 @@ use bevy::prelude::*;
 
 mod systems;
 
+#[derive(States, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum LevelState {
+    FirstWave,
+    SecondWave,
+    ThirdWave,
+}
+
 pub struct LevelPlugin;
 
 impl Plugin for LevelPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_state(LevelState::FirstWave);
         app.add_systems(Startup, spawn_background);
     }
 }
