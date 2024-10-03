51f6992bb13f00c24b38b8aaa6f607d44c6687a1
claudijo <claudijo.borovic@gmail.com>
2024-10-03 09:07:26 +0300

    Add lamp post graphics

--- src/backgrounds/mod.rs
+++ src/backgrounds/mod.rs
@@ -1,4 +1,4 @@
-use crate::backgrounds::systems::spawn_background;
+use crate::backgrounds::systems::{spawn_background, spawn_lamp_posts};
 use bevy::prelude::*;
 
 mod systems;
@@ -8,5 +8,6 @@ pub struct LevelPlugin;
 impl Plugin for LevelPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_background);
+        app.add_systems(Startup, spawn_lamp_posts);
     }
 }
--- src/backgrounds/systems.rs
+++ src/backgrounds/systems.rs
@@ -4,8 +4,17 @@ use bevy::prelude::*;
 pub fn spawn_background(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(SpriteBundle {
         texture: asset_server.load("images/background.png"),
-        transform: Transform::from_xyz(0., 64. * ASSETS_SCALE_FACTOR, 0.)
-            .with_scale(Vec3::ONE * ASSETS_SCALE_FACTOR),
+        transform: Transform::from_xyz(0., 192., 0.)
+            .with_scale(Vec3::ONE * 3.),
+        ..default()
+    });
+}
+
+pub fn spawn_lamp_posts(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("images/lamp_post.png"),
+        transform: Transform::from_xyz(-480., 30., 3.)
+            .with_scale(Vec3::ONE * 2.),
         ..default()
     });
 }
